1ef316d445ada2eaa22db90acd3ecc0f01df0225
Pon Prakash <pon.prakash@electrolux.com>
2023-11-21 18:13:52 +0530

    rich text

--- lib/flutt_learner/tutor.dart
+++ lib/flutt_learner/tutor.dart
@@ -9,24 +9,34 @@ class learnToBuild extends StatelessWidget {
     double _volume = 0;
     return Scaffold(
       appBar: AppBar(
-        title: Text("custom Ink Widget",
+        title: Text("Rich Text Widget",
         style: TextStyle( 
           color: Colors.redAccent
         ),),
         backgroundColor: Colors.tealAccent,
       ),
-      body:Center(
-        child: Ink(
-          decoration: ShapeDecoration(
-            color: Colors.blueAccent,
-            shape: CircleBorder()
-            ),
-            child: IconButton(
-              icon: Icon(Icons.add),
-              color: Colors.white,
-              onPressed: (){},),
-        ),
-      ),
+      body:RichText(
+        text: TextSpan(
+          text: 'hey from hello to',
+          style: TextStyle(
+            color: Colors.purple,
+            fontSize: 29.0,
+            fontStyle: FontStyle.italic,
+            decoration: TextDecoration.underline,
+            decorationStyle: TextDecorationStyle.dotted
+          ),
+          children: <TextSpan>[
+            TextSpan(text: 'computer',
+            style: TextStyle(
+              color: Colors.black,
+              decoration: TextDecoration.none,
+              fontWeight: FontWeight.bold,
+              fontStyle: FontStyle.normal
+            )),
+            TextSpan(text: 'from'),
+          ]
+          )
+          ),
       );
   }
 }
