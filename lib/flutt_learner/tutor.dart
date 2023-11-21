22b35c1033708c44fd2c2712fd0a19ff77d3bb55
Pon Prakash <pon.prakash@electrolux.com>
2023-11-21 17:42:13 +0530

    raised button to elevated button

--- lib/flutt_learner/tutor.dart
+++ lib/flutt_learner/tutor.dart
@@ -8,34 +8,57 @@ class learnToBuild extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Text Widget",
+        title: Text("Elevated Button Widget",
         style: TextStyle(
           color: Colors.redAccent
         ),),
         backgroundColor: Colors.tealAccent,
       ),
       body:Center(
-        child: TextButton(
-          style: ButtonStyle(
-            backgroundColor:MaterialStateProperty.all<Color>(Colors.deepOrangeAccent),
-        //      overlayColor: MaterialStateProperty.resolveWith<Color?>(
-        // (Set<MaterialState> states) {
-        //   if (states.contains(MaterialState.hovered))
-        //     return const Color.fromARGB(255, 47, 33, 243).withOpacity(0.04);
-        //   if (states.contains(MaterialState.focused) ||
-        //       states.contains(MaterialState.pressed))
-        //     return const Color.fromARGB(255, 72, 33, 243).withOpacity(0.12);
-        //   return null; // Defer to the widget's default.
-        // },
+         child: Container(
+            height: 44.0,
+            decoration: BoxDecoration(
+                gradient: LinearGradient(
+                    colors: [Color.fromARGB(255, 2, 173, 102), Colors.blue])),
+            child: ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.transparent,
+                  shadowColor: Colors.transparent),
+              child: Text('Elevated Button'),
+            ),
+          ),
+        
+        // child: ElevatedButton(
+        //   style: ButtonStyle(
+            
         //   ),
-            overlayColor: MaterialStateColor.resolveWith((states) => const Color.fromARGB(255, 73, 54, 244)),
-          ),
-          onPressed: (){}, 
-          child: Text("Click Me",
-          style: TextStyle(color: Colors.black,
-          fontSize: 22,),
-          )
-          ),
+        //   onPressed: (){},
+        //   child:Container(
+        //     // child: Text("Get Me",
+        //     // style: TextStyle(
+        //     //   color: Colors.white,
+        //     // ),),
+        //     decoration: BoxDecoration(
+        //       gradient: LinearGradient(colors: <Color>[
+        //         Color(0xFF0D47A1),
+        //         Color(0xFF1976D2),
+        //         Color(0xFF42A5F5)
+        //       ]),
+        //     ),
+        //   ) ),
+        // child: ElevatedButton(
+        //   style: ButtonStyle(
+        //     backgroundColor: MaterialStateProperty.all<Color>(Colors.amberAccent),
+        //     overlayColor: MaterialStateColor.resolveWith((states) => Colors.red),
+        //   ),
+        //   onPressed: (){}, 
+        //   child: Text("data",
+        //   style: TextStyle(
+        //     fontSize: 20,
+        //     color: Colors.black
+        //   ),
+        //   )),
       ),
       );
   }
