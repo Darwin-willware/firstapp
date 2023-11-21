0e525d9bf058cbec081e8aaec707caac5c9ab913
Pon Prakash <pon.prakash@electrolux.com>
2023-11-21 17:53:34 +0530

    icon button widget

--- lib/flutt_learner/tutor.dart
+++ lib/flutt_learner/tutor.dart
@@ -6,60 +6,33 @@ class learnToBuild extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    double _volume = 0;
     return Scaffold(
       appBar: AppBar(
-        title: Text("Elevated Button Widget",
+        title: Text("Icon Button Widget",
         style: TextStyle(
           color: Colors.redAccent
         ),),
         backgroundColor: Colors.tealAccent,
       ),
-      body:Center(
-         child: Container(
-            height: 44.0,
-            decoration: BoxDecoration(
-                gradient: LinearGradient(
-                    colors: [Color.fromARGB(255, 2, 173, 102), Colors.blue])),
-            child: ElevatedButton(
-              onPressed: () {},
-              style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.transparent,
-                  shadowColor: Colors.transparent),
-              child: Text('Elevated Button'),
-            ),
-          ),
-        
-        // child: ElevatedButton(
-        //   style: ButtonStyle(
-            
-        //   ),
-        //   onPressed: (){},
-        //   child:Container(
-        //     // child: Text("Get Me",
-        //     // style: TextStyle(
-        //     //   color: Colors.white,
-        //     // ),),
-        //     decoration: BoxDecoration(
-        //       gradient: LinearGradient(colors: <Color>[
-        //         Color(0xFF0D47A1),
-        //         Color(0xFF1976D2),
-        //         Color(0xFF42A5F5)
-        //       ]),
-        //     ),
-        //   ) ),
-        // child: ElevatedButton(
-        //   style: ButtonStyle(
-        //     backgroundColor: MaterialStateProperty.all<Color>(Colors.amberAccent),
-        //     overlayColor: MaterialStateColor.resolveWith((states) => Colors.red),
-        //   ),
-        //   onPressed: (){}, 
-        //   child: Text("data",
-        //   style: TextStyle(
-        //     fontSize: 20,
-        //     color: Colors.black
-        //   ),
-        //   )),
-      ),
+      body:Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children:<Widget> [
+         Center(
+           child: IconButton(
+            onPressed: (){
+              _volume +=10;
+            },
+             icon: Icon(Icons.volume_up),
+             iconSize: 50,
+             color: Colors.blueAccent,),
+         ),
+         Text('Volume: ${_volume}',
+         style: TextStyle(
+          fontSize: 24,
+         ),)
+        ],
+      ) ,
       );
   }
 }
