d7ab9bf92e795b4b680f71a7b043fa7164a87282
Pon Prakash <pon.prakash@electrolux.com>
2023-11-22 15:55:21 +0530

    navigation page routing

--- lib/flutt_learner/tutor.dart
+++ lib/flutt_learner/tutor.dart
@@ -1,62 +1,84 @@
-
-
 import 'package:flutter/material.dart';
 
-class learnToBuild extends StatefulWidget {
+class learnToBuild extends StatelessWidget {
   const learnToBuild({super.key});
 
   @override
-  State<learnToBuild> createState() => _learnToBuildState();
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: FirstPage(),
+    );
+  }
 }
 
-class _learnToBuildState extends State<learnToBuild> {
-    bool mon = false;
-    bool tue = false;
-    bool wed = false;
+class FirstPage extends StatelessWidget {
+  const FirstPage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-         appBar: AppBar(
-          backgroundColor: Colors.tealAccent,
-          title: Text("CheckBox Widget",
-          style: TextStyle(
-            color: Colors.redAccent,
-          ),),
-         ),
-         body: Center(
-           child: Row(
-             children:<Widget> [
-               customCheckbox('Mon',mon),
-               customCheckbox('Tue', tue),
-               customCheckbox('Wed', wed)
-             ],
-           ),
-         ),
-   );
+      appBar: AppBar(
+        title: Text("Page Routing",
+        style: TextStyle(
+          color: Colors.redAccent,
+        ),
+        ),
+        backgroundColor: Colors.tealAccent,
+      ),
+      body: Center(
+        child: Text(" of Page 001",
+        style: TextStyle(
+          fontSize: 25.0,
+          fontWeight: FontWeight.bold,
+        ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.navigate_next),
+        onPressed: (){
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (context)=>
+              SecondPage()
+              )
+          );
+        }),
+    );
   }
+}
 
-  Column customCheckbox(String title,bool val) {
-    return Column(
-              children:<Widget> [
-                Text(title),
-                Checkbox(value: val,
-                 onChanged: (bool? value){
-                  setState(() {
-                    switch(title){
-                      case 'Mon':
-                      mon = value!;
-                      break;
-                      case 'Tue':
-                      tue = value!;
-                      break;
-                      case 'Wed':
-                      wed = value!;
-                      break;
-                    }
-                  });
-                 },
-                 )
-              ],
-             );
+class SecondPage extends StatelessWidget {
+  const SecondPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+     return Scaffold(
+      appBar: AppBar(
+        title: Text("Page Routing",
+        style: TextStyle(
+          color: Colors.redAccent,
+        ),
+        ),
+        backgroundColor: Colors.tealAccent,
+      ),
+      body: Center(
+        child: Text(" of Page 002",
+        style: TextStyle(
+          fontSize: 25.0,
+          fontWeight: FontWeight.bold,
+        ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.navigate_next),
+        onPressed: (){
+          Navigator.of(context).pop(
+            MaterialPageRoute(
+              builder: (context)=>
+              FirstPage()
+              )
+          );
+        }),
+    );
   }
 }
