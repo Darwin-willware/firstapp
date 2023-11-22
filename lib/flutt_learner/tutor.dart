318685121f3f544e1b11ef4709d630514313e707
Pon Prakash <pon.prakash@electrolux.com>
2023-11-22 12:41:21 +0530

    image widget

--- lib/flutt_learner/tutor.dart
+++ lib/flutt_learner/tutor.dart
@@ -9,24 +9,38 @@ class learnToBuild extends StatelessWidget {
     double _volume = 0;
     return Scaffold(
       appBar: AppBar(
-        title: Text("Button Bars Widget",
+        title: Text("image Widget",
         style: TextStyle( 
           color: Colors.redAccent
         ),),
         backgroundColor: Colors.tealAccent,
       ),
       body:Container(
-        child: ButtonBar(
-          alignment: MainAxisAlignment.center,
-          
-          children:<Widget> [
-            TextButton(onPressed: (){}, child: Text("login")),
-             IconButton(onPressed: (){}, icon: Icon(Icons.home)),
-            IconButton(onPressed: (){}, icon: Icon(Icons.add)),
-            IconButton(onPressed: (){}, icon: Icon(Icons.people)),
-            IconButton(onPressed: (){}, icon: Icon(Icons.search)),
-          ],
+        height: 150,
+        width: 300,
+        decoration: BoxDecoration(
+          border: Border.all(width: 2.0),
+          borderRadius: BorderRadius.circular(5.0),
+          image: DecorationImage(
+            image: NetworkImage(
+              'https://media.gcflearnfree.org/ctassets/topics/246/share_flower_fullsize.jpg'
+              ),
+              ),
         ),
+        child: Center(
+          child: Text('Flower',
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold
+          ),),
+        ),
+        // child: Image.network(
+        //   'https://media.gcflearnfree.org/ctassets/topics/246/share_flower_fullsize.jpg',
+        //   fit: BoxFit.fill,
+        // ),
+        //width: double.infinity,
+        //child: Image(image:AssetImage('assets/images/oneimg.jpg'),fit: BoxFit.cover,),
+       
       ),
       );
   }
