3be1de3e8a69eb725344bfc20aa0df18dd9fc144
Pon Prakash <pon.prakash@electrolux.com>
2023-11-22 13:21:11 +0530

    bottom sheet widget like alert in frontend

--- lib/flutt_learner/tutor.dart
+++ lib/flutt_learner/tutor.dart
@@ -1,51 +1,62 @@
 
+
 import 'package:flutter/material.dart';
 
-class learnToBuild extends StatelessWidget {
+class learnToBuild extends StatefulWidget {
   const learnToBuild({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    double _volume = 0;
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("card Widget",
-        style: TextStyle( 
-          color: Colors.redAccent
-        ),),
-        backgroundColor: Colors.tealAccent,
-      ),
-      body:Container(
-           child: Center(
-             child: Card(
-              color: Color.fromARGB(179, 15, 178, 219),
-              elevation: 10.0,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(5.0),
-              ),
-              child: Container(
-                height: 100,
-                padding: EdgeInsets.all(15.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children:<Widget> [
-                    Text('Try not to become a man of success, but rather try to become a man of value',
-                    style: TextStyle(
-                      fontSize: 15.0,
-                    ),
-                    ),
-                    Text('-Albert Einstein',
-                    style: TextStyle(
-                      fontSize: 18.0,
-                      fontWeight: FontWeight.bold
-                    ),
-                    ),
-                  ],
+  State<learnToBuild> createState() => _learnToBuildState();
+}
+
+class _learnToBuildState extends State<learnToBuild> {
+  void showBottom(){
+    showModalBottomSheet<void>(
+      context: context, 
+      builder: (BuildContext context){
+         return Container(
+          padding: EdgeInsets.all(20.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children:<Widget> [
+               Text("Welcome To G7 Learning Portal"),
+               ElevatedButton(
+                onPressed: (){
+                  Navigator.pop(context);
+                },
+                child: Text(
+                  "Close Me"
                 ),
-              ),
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all<Color>(Colors.redAccent),
+                ),
+                )
+            ],
+          ),
+         );
+      });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+         appBar: AppBar(
+          backgroundColor: Colors.tealAccent,
+          title: Text("Bottom sheet(Alert) Widget",
+          style: TextStyle(
+            color: Colors.redAccent,
+          ),),
+         ),
+         body: Center(
+          child: ElevatedButton(onPressed: showBottom, 
+          child:Text(
+            "Click Me",
+             style: TextStyle(
+              fontSize: 18.0
              ),
+          )
            ),
-      ),
-      );
+         ),
+
+    );
   }
 }
