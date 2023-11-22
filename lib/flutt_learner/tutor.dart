7719ee9d141f635e3092af6422be1f7904bc07ef
Pon Prakash <pon.prakash@electrolux.com>
2023-11-22 14:48:56 +0530

    textfield widget

--- lib/flutt_learner/tutor.dart
+++ lib/flutt_learner/tutor.dart
@@ -10,100 +10,42 @@ class learnToBuild extends StatefulWidget {
 }
 
 class _learnToBuildState extends State<learnToBuild> {
-  void showBottom(){
-    showModalBottomSheet<void>(
-      context: context, 
-      builder: (BuildContext context){
-         return Container(
-          padding: EdgeInsets.all(20.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children:<Widget> [
-               Text("Welcome To G7 Learning Portal"),
-               ElevatedButton(
-                onPressed: (){
-                  Navigator.pop(context);
-                },
-                child: Text(
-                  "Close Me"
-                ),
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all<Color>(Colors.redAccent),
-                ),
-                )
-            ],
-          ),
-         );
+    String _value = '';
+    void _onChanged(String value){
+      setState(() {
+        _value = value;
       });
-  }
+    }
+    void _onSubmit(String value){
+        setState(() {
+          _value = 'submit:${value}';
+        });
+    }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
          appBar: AppBar(
           backgroundColor: Colors.tealAccent,
-          title: Text("Single child Scroll view Widget",
+          title: Text("TextField Widget",
           style: TextStyle(
             color: Colors.redAccent,
           ),),
          ),
-         body: SingleChildScrollView(
-          scrollDirection: Axis.horizontal,
-           child: Row(
-            children:<Widget> [
-             Container(
-              height: 200,
-              width: 150,
-              color: Colors.deepOrangeAccent,
-             ),
-             SizedBox(
-              height: 10.0,
-              width: 10.0,
-             ),
-             Container(
-              height: 200,
-              width: 150,
-              color: Colors.tealAccent,
-             ),
-             SizedBox(
-              height: 10.0,
-              width: 10.0,
-             ),
-             Container(
-              height: 200,
-              width: 150,
-              color: Colors.indigoAccent,
-             ),
-             SizedBox(
-              height: 10.0,
-              width: 10.0,
-             ),
-             Container(
-              height: 200,
-              width: 150,
-              color: Colors.cyanAccent,
-             ),
-             SizedBox(
-              height: 10.0,
-              width: 10.0,
-             ),
-             Container(
-              height: 200,
-              width: 150,
-              color: Colors.lightGreenAccent,
-             ),
-             SizedBox(
-              height: 10.0,
-              width: 10.0,
-             ),
-             Container(
-              height: 200,
-              width: 200,
-              color: Colors.blueAccent,
-             ),           
-            ],
-           ),
+         body: Column(
+          children:<Widget> [
+              Text(_value),
+              TextField(
+                decoration: new InputDecoration(
+                  labelText: "Enter Number",
+                  hintText: 'placeholder',
+                  icon: new Icon(Icons.people)),
+                  autocorrect: true,
+                onChanged: _onChanged,
+                onSubmitted: _onSubmit,
+                //keyboardType: TextInputType.number,
+              )
+          ],
          ),
-
-    );
+   );
   }
 }
