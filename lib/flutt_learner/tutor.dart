72a9e286749e1991811ff65e6916c8fc6f4122fe
Pon Prakash <pon.prakash@electrolux.com>
2023-11-23 10:51:46 +0530

    show search bar with suggestions in widget class

--- lib/flutt_learner/tutor.dart
+++ lib/flutt_learner/tutor.dart
@@ -12,25 +12,93 @@ class _learnToBuildState extends State<learnToBuild> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: 
-      Text("FittedBox Widget",style: TextStyle(color: Colors.black),), 
-      elevation: 0.0, backgroundColor: Colors.transparent,),
-      body: Center(
-        child: Container(
-          width: 300,
-          height: 50,
-          color: Colors.red,
-          padding: EdgeInsets.all(10.0),
-          child: FittedBox(
-            child: const Text("FLUTTER MAPP",
-            style: TextStyle(
-              fontSize: 100,
-              color: Colors.white,
-              fontWeight: FontWeight.bold
-            ),),
-          ),
-
+      Text("showSearch Widget",
+      style: TextStyle(
+        color: Colors.black
         ),
-      ),
+        ), 
+      elevation: 0.0,
+       backgroundColor: Colors.transparent,
+       actions: [
+        IconButton(onPressed: (){
+          showSearch(context: context,
+           delegate: CustomSearchDelegate(),);
+        }, 
+        icon: const Icon(Icons.search),
+        )
+       ],
+       ),
+      
     );
+  }
+}
+
+class CustomSearchDelegate extends SearchDelegate {
+  List<String> searchterms= [
+    'Apple',
+    'Banana',
+    'Pear',
+    'Watermelons',
+    'Oranges',
+    'BlueBerries',
+    'Strawberries',
+    'Raspberries',
+  ];
+  @override
+  List<Widget>? buildActions(BuildContext context) {
+        return [
+          IconButton(
+            onPressed: () {
+              query = '';
+            },
+           icon: const Icon(Icons.clear),
+           ), 
+        ];
+  }
+
+  @override
+  Widget? buildLeading(BuildContext context) {
+         return IconButton(onPressed: () {
+          close(context, null);
+         }, icon: const Icon(Icons.arrow_back)
+         );
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+             List<String> matchQuery = [];
+             for(var fruit in searchterms){
+              if(fruit.toLowerCase().contains(query.toLowerCase())){
+                matchQuery.add(fruit);
+              }
+             }
+             return ListView.builder(
+              itemCount: matchQuery.length,
+              itemBuilder: (context, index){
+                var result = matchQuery[index];
+                return ListTile(
+                  title: Text(result),
+                );
+              }
+              );
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+        List<String> matchQuery = [];
+        for(var fruit in searchterms){
+              if(fruit.toLowerCase().contains(query.toLowerCase())){
+                matchQuery.add(fruit);
+              }
+             }
+              return ListView.builder(
+              itemCount: matchQuery.length,
+              itemBuilder: (context, index){
+                var result = matchQuery[index];
+                return ListTile(
+                  title: Text(result),
+                );
+              }
+              );
   }
 }
