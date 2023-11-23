48f011878ba63555164813893c705e573ce472a3
Pon Prakash <pon.prakash@electrolux.com>
2023-11-23 12:38:56 +0530

    PopMenu Widget

--- lib/flutt_learner/tutor.dart
+++ lib/flutt_learner/tutor.dart
@@ -7,119 +7,35 @@ class learnToBuild extends StatefulWidget {
   State<learnToBuild> createState() => _learnToBuildState();
 }
 
-
-//FAKE FIREBASE STREAM TO GET STREAM OF INPUTS ONE AFTER ANOTHEER
-Stream<int> generateStream = (() async*{
-  await Future<void>.delayed(const Duration(seconds:2));
-  yield 1;
-  await Future<void>.delayed(const Duration(seconds:4));
-  yield 2;
-  await Future<void>.delayed(const Duration(seconds:1));
-  yield 3;
-})();
-
 class _learnToBuildState extends State<learnToBuild> {
-  DateTime _dateTime = DateTime(2100);
+  String title = "Pop Menu Widget";
+  String firstPage = "First Page";
+    String secondPage = "Second Page";
   @override
   Widget build(BuildContext context) {
 
     return Scaffold(
       appBar: AppBar(
-        title: Text("Show Date Picker  Widget"),
-      ),
-     body: Center(
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Text(
-            '${_dateTime.day}-${_dateTime.month}-${_dateTime.year}',
-            style: const TextStyle(fontSize: 40),
-          ),
-          ElevatedButton(
-            onPressed: () async{
-              DateTime? _newDate = await showDatePicker(context: context,
-               initialDate: _dateTime, 
-               firstDate: DateTime(1800), lastDate: DateTime(3000));
-               if(_newDate != null){
-                setState(() {
-                  _dateTime = _newDate;
-                });
-               }
-            }, 
-            child: const Text("Get Date"))
+        title: Text(title),
+        actions: [
+          PopupMenuButton(itemBuilder: (context)=>[
+            PopupMenuItem(child: Text(firstPage),
+            value: firstPage,
+            ),
+            PopupMenuItem(child: Text(secondPage),
+            value: secondPage,
+            )
+          ],
+          onSelected: (String newValue){
+               setState(() {
+                 title = newValue;
+               });
+          },
+          )
         ],
       ),
-     ),
+     
     );
   }
 }
 
-class CustomSearchDelegate extends SearchDelegate {
-  List<String> searchterms= [
-    'Apple',
-    'Banana',
-    'Pear',
-    'Watermelons',
-    'Oranges',
-    'BlueBerries',
-    'Strawberries',
-    'Raspberries',
-  ];
-  @override
-  List<Widget>? buildActions(BuildContext context) {
-        return [
-          IconButton(
-            onPressed: () {
-              query = '';
-            },
-           icon: const Icon(Icons.clear),
-           ), 
-        ];
-  }
-
-  @override
-  Widget? buildLeading(BuildContext context) {
-         return IconButton(onPressed: () {
-          close(context, null);
-         }, icon: const Icon(Icons.arrow_back)
-         );
-  }
-
-  @override
-  Widget buildResults(BuildContext context) {
-             List<String> matchQuery = [];
-             for(var fruit in searchterms){
-              if(fruit.toLowerCase().contains(query.toLowerCase())){
-                matchQuery.add(fruit);
-              }
-             }
-             return ListView.builder(
-              itemCount: matchQuery.length,
-              itemBuilder: (context, index){
-                var result = matchQuery[index];
-                return ListTile(
-                  title: Text(result),
-                );
-              }
-              );
-  }
-
-  @override
-  Widget buildSuggestions(BuildContext context) {
-        List<String> matchQuery = [];
-        for(var fruit in searchterms){
-              if(fruit.toLowerCase().contains(query.toLowerCase())){
-                matchQuery.add(fruit);
-              }
-             }
-              return ListView.builder(
-              itemCount: matchQuery.length,
-              itemBuilder: (context, index){
-                var result = matchQuery[index];
-                return ListTile(
-                  title: Text(result),
-                );
-              }
-              );
-  }
-}
