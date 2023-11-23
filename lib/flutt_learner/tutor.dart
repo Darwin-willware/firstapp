64ccef7b120938b6fe46f8dc6a785cb92af241f0
Pon Prakash <pon.prakash@electrolux.com>
2023-11-23 15:12:10 +0530

    Futurebuilder Widget to build view async

--- lib/flutt_learner/tutor.dart
+++ lib/flutt_learner/tutor.dart
@@ -1,5 +1,6 @@
 
 import 'package:flutter/material.dart';
+import 'package:path/path.dart';
 
 class learnToBuild extends StatefulWidget {
   const learnToBuild({super.key});
@@ -7,31 +8,52 @@ class learnToBuild extends StatefulWidget {
   @override
   State<learnToBuild> createState() => _learnToBuildState();
 }
-
+  
+  Future<String> getData() async{
+    await Future.delayed(const Duration(seconds: 1));
+    // throw 'An Error Occured'
+    return 'It Works';
+  }
 class _learnToBuildState extends State<learnToBuild> {
-  int _currentIndex = 0;
-   bool _bool = true;
-  List<Widget> body  = const [
-    Icon(Icons.home),
-    Icon(Icons.video_file),
-    Icon(Icons.add),
-    Icon(Icons.subscriptions),
-  ];
+ 
   @override
   Widget build(BuildContext context) {
    //WillpopScope is used to disable the back button on Android devices
-    return WillPopScope(
-      child: Scaffold(
-        backgroundColor: Colors.black,
-        appBar: AppBar(
-          title: const Text("WillPopeScope Widget"),
+    return Scaffold(
+      //this widget will wait for data before displaying something on view 
+      //Futurebuilder is used to build the view dynamically depends upon the data that we get asynchronously
+      appBar: AppBar(
+        title: const Text("FutureBuilder",
+        style: TextStyle(color: Colors.black),
         ),
+        backgroundColor: Colors.transparent,
       ),
-     onWillPop: () async{
-      //false to disable the back button in android
-      return true;
-     }
-     );
+      body: Center(
+        child: FutureBuilder(
+          future: getData(),
+           builder: (Context,snapshot){
+               if(snapshot.connectionState == ConnectionState.waiting){
+                 return const CircularProgressIndicator.adaptive();
+               }
+               if(snapshot.hasError){
+                    return Text(snapshot.error.toString());
+               } else {
+                return Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text(snapshot.data.toString(),),
+                    ElevatedButton(onPressed: (){
+                      setState(() {
+                        
+                      });
+                    }, child: const Text("refresh"))
+                  ],
+                );
+               }
+           }
+           ),
+      ),
+    );
      
   }
 }
